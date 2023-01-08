04fc7c9f25826ddee43c59746a171756efce3e24
Maya <DahirowSumaia@gmail.com>
2023-01-08 17:30:52 -0600

    made changes to css snd JS

--- script.js
+++ script.js
@@ -1,66 +1,58 @@
 $('document').ready(readyNow)
 
 function readyNow(){
-    appendDOM();
 $('#submit-button').on("click", submitButton); 
-$("#empInfo").on("click", "button", print); 
-}
-const employeeInfo = [];
-let sumOfEmpAnnualSalary = 0;
+$("#empInfo").on("click", "button", deleteEmployee);
+$("#empInfo").on("click", () => $("#empInfo").empty());
+sumOfEmployeeAnnualSalary = 0;
+$('#total-annualsalary').text(sumOfEmployeeAnnualSalary);
+}; 
 
-function print(event) {
+let sumOfEmployeeAnnualSalary = 0;
+function submitButton( ){
+    let sumOfEmployeeAnnualSalary = 0;   
+     const newObj = {
+         firstName : $('#employee-firstname').val(),
+         lastName : $('#employee-lastname').val(),
+         idNumber : $('#employee-idnumber').val(),
+         jobTitle : $('#employee-jobtitle').val(),
+         annualSalary : $('#employee-annualsalary').val()
+     };
+
+     console.log(newObj);
+     $('#employee-firstname').val();
+     $('#employee-lastname').val();
+     $('#employee-idnumber').val();
+     $('#employee-jobtitle').val();
+     $('#employee-annualsalary').val();
+     
+     if (Number(newObj.sumOfEmployeeAnnualSalary) > 20000) {
+        $('#total-monthly-cost').css('background-color', 'red');
+     }
+         $("#empInfo").append(
+            `<tr class="${empRow}">
+               <td>${newObj.firstName}</td>
+               <td>${newObj.lastName}</td>
+               <td>${newObj.idNumber}</td>
+               <td>${newObj.jobTitle}</td>
+               <td>${newObj.annualSalary}</td>
+               <td><button id="#empInfo">Delete</button></td>
+              </tr>
+              `
+              );
+
+sumOfEmployeeAnnualSalary += Number(newObj.annualSalary.sumOfEmployeeAnnualSalary);
+ $('#total-annualsalary').text(Number(sumOfEmployeeAnnualSalary));           
+      
+    }
+    
+function deleteEmployee(event) {
+    /*let count = $(event.target).closest('tr').find('h3').text();
+    sumOfEmployeeAnnualSalary -= Number(count);
+    $('#total-annualsalary').text(Number(sumOfEmployeeAnnualSalary));
+    */
     $(event.target).closest("tr").remove();
-}
+    }
 
-function appendDOM(){
-    $("#empInfo").empty()
-    for (let i = 0; i < employeeInfo.length; i++) {
-        let el = employeeInfo[i];
-        
-    $("#empInfo").append(
-        `<tr>
-           <td>${el.firstName}</td>
-           <td>${el.lastName}</td>
-           <td>${el.idNumber}</td>
-           <td>${el.jobTitle}</td>
-           <td>${el.annualSalary}</td>
-           <td>
-           <button id="#empInfo">Delete</button>
-           </td>
-          </tr>
-          `
-          );
-          } }
 
-function submitButton(){
-   let firstName = $('#employee-firstname').val();
-    let lastName = $('#employee-lastname').val();
-    let idNumber = $('#employee-idnumber').val();
-    let jobTitle = $('#employee-jobtitle').val();
-    let annualSalary = $('#employee-annualsalary').val();
 
-    addEmployeesInfo(firstName, lastName, idNumber, jobTitle, annualSalary)
-    $('#employee-firstname').val();
-    $('#employee-lastname').val();
-    $('#employee-idnumber').val();
-    $('#employee-jobtitle').val();
-    $('#employee-annualsalary').val();
-    $('#sum').text();
-    appendDOM()
-}
-
-function addEmployeesInfo(firstName, lastName, idNumber, jobTitle, annualSalary){
-    console.log(`Employee information ${firstName} ${lastName} ${idNumber}  ${jobTitle}  ${annualSalary}`);
-const newObj = {
-    firstName: firstName,
-    lastName: lastName,
-    idNumber: idNumber,
-    jobTitle: jobTitle,
-    annualSalary: annualSalary
-}
-employeeInfo.push(newObj);
-}
-
-function sumOfAnnualSalary(){
-
-}
