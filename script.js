7d4c8459e7c9a70aa435263d7335c37fba94aad0
Maya <DahirowSumaia@gmail.com>
2023-01-08 14:25:01 -0600

    added button on js and appendDOM function

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 $('document').ready(readyNow)
 
 function readyNow(){
+    appendDOM();
+$('#submit-button').on("click", submitButton);
     
+}
+
+function appendDOM(){
+    $("#empInfo").append(
+        `<tr>
+           <td>${firstName}</td>
+           <td>${lastName}</td>
+           <td>${idNumber}</td>
+           <td>${jobTitle}</td>
+           <td>${annualSalary}</td>
+          </tr>`);
+          }
+
+
+function submitButton(){
+    let firstName = $('#employee-firstname').val();
+    let lastName = $('#employee-lastname').val();
+    let idNumber = $('#employee-idnumber').val();
+    let jobTitle = $('#employee-jobtitle').val();
+    let annualSalary = $('#employee-annualsalary').val();
+}
+
+function addEmployeesInfo(firstName, lastName, idNumber, jobTitle, annualSalary){
+    console.log(`Employee information ${firstName} ${lastName} ${idNumber}  ${jobTitle}  ${annualSalary}`);
+
 }
